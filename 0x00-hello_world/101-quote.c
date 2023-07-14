ab389f8bee4c5bdf4167bd5be55a87331f0b000b
abdelhamid2004 <abdelhamideslam900@gmail.com>
2023-07-13 19:28:02 -0700

    acc

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main -A c pogram that prints a line to the standerd error
+*Return: always 1 (success)
+     */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
