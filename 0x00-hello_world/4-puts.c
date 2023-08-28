e2c2186f131686c7ac8fea7f729daf170c2bcb2c
abdelhamid2004 <abdelhamideslam900@gmail.com>
2023-08-28 04:28:41 -0700

    acc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main -Entry point
-*Return: always 0 (success)
+* main - A progran for printing with a new line
+* Return: 0 (Success)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
