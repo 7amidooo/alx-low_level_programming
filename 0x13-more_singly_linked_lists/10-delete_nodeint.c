e4074ef3604783eb842703aac1da1fc006137397
abdelhamid2004 <abdelhamideslam900@gmail.com>
2023-08-29 11:19:15 -0700

    acc

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp = *head;
 	listint_t *current = NULL;
 	unsigned int i = 0;
-if (*head == NULL)
+
+	if (*head == NULL)
 		return (-1);
 
 	if (index == 0)
